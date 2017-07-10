79a9bb6b24f4c512ad274d0512f8867be791a9f2
nibal abazli <alaa.hiba4@gmail.com>
2017-07-10 16:29:00 +0300

    directive and connecting pages

---
+++ public/js/dirc.js
@@ -0,0 +1,10 @@
+angular.module("TodoApp").directive("myDirc", function () {
+    return {
+        restrict: "E",
+        templateUrl: "./views/dirc.html",
+        link: function (scope, element, attr) {
+
+        }
+    }
+})
+
